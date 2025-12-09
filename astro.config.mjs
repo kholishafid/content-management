089e97d40d0f242b98b865b319d3ea296ce5a5c2
kholishafid <hafidnkholis@gmail.com>
2025-12-09 22:31:26 +0700

    feat: add cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,15 @@ import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), markdoc(), keystatic()],
+
   server: {
      port: 4322, // Set your desired port here
    },
+
+  adapter: cloudflare(),
 });
